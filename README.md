a870eaaf6bec5f6bd674572ee86e1bc4bd950263
Anthony Sottile <asottile@umich.edu>
2017-08-02 10:36:37 -0700

    Initial skeleton

---
+++ README.md
@@ -0,0 +1,25 @@
+[![Build Status](https://travis-ci.org/asottile/all-repos.svg?branch=master)](https://travis-ci.org/asottile/all-repos)
+[![Coverage Status](https://coveralls.io/repos/github/asottile/all-repos/badge.svg?branch=master)](https://coveralls.io/github/asottile/all-repos?branch=master)
+
+all-repos
+=========
+
+Clone all your repositories and apply sweeping changes.
+
+## Installation
+
+`pip install all-repos`
+
+
+## Configuring
+
+TODO
+
+
+## Usage
+
+TODO
+
+## Built-in refactoring apis
+
+TODO
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='all_repos',
+    description='Clone all your repositories and apply sweeping changes.',
+    url='https://github.com/asottile/all-repos',
+    version='0.0.0',
+    author='Anthony Sottile',
+    author_email='asottile@umich.edu',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[],
+    packages=find_packages(exclude=('tests*',)),
+    entry_points={'console_scripts': []},
+)
