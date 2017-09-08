122cbf88d9abe4f9ee3241a85582af109ebc90f7
Anthony Sottile <asottile@umich.edu>
2017-09-07 19:14:05 -0700

    Better filter for pre-commit migrate-config

--- tests/autofix/pre_commit_migrate_config_test.py
+++ tests/autofix/pre_commit_migrate_config_test.py
@@ -1,4 +1,8 @@
+from all_repos import clone
+from all_repos.autofix.pre_commit_migrate_config import find_repos
 from all_repos.autofix.pre_commit_migrate_config import main
+from all_repos.config import load_config
+from testing.git import write_file_commit
 
 
 def test_main(file_config, autoupdatable):
@@ -16,3 +20,17 @@ def test_main(file_config, autoupdatable):
         f'    hooks:\n'
         f'    -   id: hook\n'
     )
+
+
+def test_find_repos(file_config_files):
+    write_file_commit(
+        # A migrated configuration
+        file_config_files.dir1, '.pre-commit-config.yaml', 'repos: []\n',
+    )
+    write_file_commit(
+        # A non-migrated configuration
+        file_config_files.dir2, '.pre-commit-config.yaml', '[]\n',
+    )
+    clone.main(('--config-filename', str(file_config_files.cfg)))
+    ret = find_repos(load_config(str(file_config_files.cfg)))
+    assert ret == {str(file_config_files.output_dir.join('repo2'))}
