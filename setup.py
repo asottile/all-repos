6443d982cd802ea55440ce35b9f6a36fdb2e04f2
Anthony Sottile <asottile@umich.edu>
2017-10-24 20:50:09 -0700

    v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='all_repos',
     description='Clone all your repositories and apply sweeping changes.',
     url='https://github.com/asottile/all-repos',
-    version='0.2.0',
+    version='1.0.0',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
