c8b8fe9af28bd0df5392b04d48705bddc46f504e
Anthony Sottile <asottile@umich.edu>
2019-02-27 21:37:49 -0800

    Require success (setup-cfg-fmt>=0.0.4)

--- all_repos/autofix/setup_py_upgrade.py
+++ all_repos/autofix/setup_py_upgrade.py
@@ -15,9 +15,7 @@ def find_repos(config: Config) -> Set[str]:
 
 def apply_fix() -> None:
     autofix_lib.run(sys.executable, '-m', 'setup_py_upgrade', '.')
-    autofix_lib.run(
-        sys.executable, '-m', 'setup_cfg_fmt', 'setup.cfg', check=False,
-    )
+    autofix_lib.run(sys.executable, '-m', 'setup_cfg_fmt', 'setup.cfg')
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
