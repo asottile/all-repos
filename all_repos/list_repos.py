b0f56af688eca0329daf33e2fee83f91a9f70e2d
Andras Mitzki <andras.mitzki@balabit.com>
2019-03-19 17:01:00 +0100

    ListRepos: Support '--output-paths' option from cli
    - using this option repos will printed with
    configured output_dir
    
    Signed-off-by: Andras Mitzki <mitzkia@gmail.com>

--- all_repos/list_repos.py
+++ all_repos/list_repos.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from typing import Optional
 from typing import Sequence
 
@@ -12,11 +13,15 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         usage='all-repos-list-repos [options]',
     )
     cli.add_common_args(parser)
+    cli.add_output_paths_arg(parser)
     args = parser.parse_args(argv)
 
     config = load_config(args.config_filename)
     for repo in config.get_cloned_repos():
-        print(repo)
+        if args.output_paths:
+            print(os.path.join(config.output_dir, repo))
+        else:
+            print(repo)
     return 0
 
 
