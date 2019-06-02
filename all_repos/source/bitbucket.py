f9a9faa656c9e639ab1bc1170cbb2bf4f019e0f6
Ben Watkins <ben@outdatedversion.com>
2019-06-01 19:04:59 -0500

    fix up some formatting issues caught by linter

--- all_repos/source/bitbucket.py
+++ all_repos/source/bitbucket.py
@@ -9,16 +9,18 @@ class Settings(NamedTuple):
     username: str
     app_password: str
 
-    def b64_encode_username_password(self):
-        return base64.b64encode(bytes(f'{self.username}:{self.app_password}', 'utf-8')).decode('ascii')
+    def b64_encode_username_password(self) -> str:
+        value = f'{self.username}:{self.app_password}'
+        return base64.b64encode(bytes(value, 'utf-8')).decode('ascii')
 
 
 def list_repos(settings: Settings) -> Dict[str, str]:
     repos = bitbucket_api.get_all(
-        'https://api.bitbucket.org/2.0/repositories?pagelen=100&role=member',
-        headers={'Authorization': f'Basic {settings.b64_encode_username_password()}'}
+        'https://api.bitbucket.org/2.0/repositories?pagelen=100&role=member',  # noqa: E501,E261
+        headers={'Authorization': f'Basic {settings.b64_encode_username_password()}'},  # noqa: E501,E261
     )
 
     return {
-        repo['full_name']: 'git@bitbucket.org:{}.git'.format(repo['full_name']) for repo in repos
+        repo['full_name']: 'git@bitbucket.org:{}.git'.format(repo['full_name'])
+        for repo in repos
     }
