f53801b96618cd814987e37f41eda4ada6c4a816
David Szotten <davidszotten@gmail.com>
2020-10-02 08:49:29 +0100

    github_api: use ssh_url instead of fixed base url
    this makes sure we get the correct url for github enterprise repos that
    use a custom domain. hopefully fixes #133

--- tests/github_api_test.py
+++ tests/github_api_test.py
@@ -1,5 +1,6 @@
 import pytest
 
+from all_repos.github_api import _strip_trailing_dot_git
 from all_repos.github_api import better_repr
 from all_repos.github_api import get_all
 from testing.mock_http import FakeResponse
@@ -52,3 +53,25 @@ def test_get_all(mock_urlopen):
 
     ret = get_all('https://example.com/api')
     assert ret == ['page1_1', 'page1_2', 'page2_1', 'page2_2', 'page3_1']
+
+
+@pytest.mark.parametrize(
+    ('val', 'expected'),
+    (
+        ('', ''),
+        (
+            'git@github.com:sass/libsass-python',
+            'git@github.com:sass/libsass-python',
+        ),
+        (
+            'git@github.com:sass/libsass-python.git',
+            'git@github.com:sass/libsass-python',
+        ),
+        (
+            'git@github.com:.git/example',
+            'git@github.com:.git/example',
+        ),
+    ),
+)
+def test_strip_trailing_dot_git(val, expected):
+    assert _strip_trailing_dot_git(val) == expected
