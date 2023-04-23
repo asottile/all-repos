eb33ab8b97b5bb41a90d6cdcae84911eaf06ffd8
Oliver Lambson <oliverlambson@gmail.com>
2023-04-23 11:53:07 +0100

    Run pre-commit

--- all_repos/config.py
+++ all_repos/config.py
@@ -4,9 +4,12 @@ import json
 import os
 import re
 import sys
-from typing import Any, Callable, NamedTuple, Pattern
+from typing import Any
+from typing import Callable
+from typing import NamedTuple
+from typing import Pattern
 
-REPOS_JSON_FILES = frozenset(("repos.json", "repos_filtered.json"))
+REPOS_JSON_FILES = frozenset(('repos.json', 'repos_filtered.json'))
 
 
 class Config(NamedTuple):
@@ -24,11 +27,11 @@ class Config(NamedTuple):
 
     @property
     def repos_path(self) -> str:
-        return self._path("repos.json")
+        return self._path('repos.json')
 
     @property
     def repos_filtered_path(self) -> str:
-        return self._path("repos_filtered.json")
+        return self._path('repos_filtered.json')
 
     def get_cloned_repos(self) -> dict[str, str]:
         with open(self.repos_filtered_path) as f:
@@ -37,10 +40,10 @@ class Config(NamedTuple):
 
 def _check_permissions(filename: str) -> None:
     mode = os.stat(filename).st_mode & 0o777
-    if sys.platform != "win32" and mode != 0o600:
+    if sys.platform != 'win32' and mode != 0o600:
         raise SystemExit(
-            f"{filename} has too-permissive permissions, Expected 0o600, "
-            f"got 0o{mode:o}",
+            f'{filename} has too-permissive permissions, Expected 0o600, '
+            f'got 0o{mode:o}',
         )
 
 
@@ -51,35 +54,37 @@ def _check_output_dir(output_dir: str) -> None:
             return
 
         if not (
-            contents >= REPOS_JSON_FILES
-            and all(
+            contents >= REPOS_JSON_FILES and
+            all(
                 os.path.isdir(os.path.join(output_dir, d))
                 for d in contents - REPOS_JSON_FILES
             )
         ):
             raise SystemExit(
-                "output_dir should only contain repos.json, "
-                "repos_filtered.json, and directories",
+                'output_dir should only contain repos.json, '
+                'repos_filtered.json, and directories',
             )
 
 
 def load_config(filename: str) -> Config:
     if not os.path.exists(filename):
-        raise SystemExit(f"{filename} does not exist. See `all-repos --help` for more.")
+        raise SystemExit(
+            f'{filename} does not exist, ' ' See `all-repos --help` for more.',
+        )
     _check_permissions(filename)
     with open(filename) as f:
         contents = json.load(f)
 
-    output_dir = os.path.join(filename, "..", contents["output_dir"])
+    output_dir = os.path.join(filename, '..', contents['output_dir'])
     output_dir = os.path.normpath(output_dir)
     _check_output_dir(output_dir)
-    source_module: Any = __import__(contents["source"], fromlist=["__trash"])
-    source_settings = source_module.Settings(**contents["source_settings"])
-    push_module: Any = __import__(contents["push"], fromlist=["__trash"])
-    push_settings = push_module.Settings(**contents["push_settings"])
-    include = re.compile(contents.get("include", ""))
-    exclude = re.compile(contents.get("exclude", "^$"))
-    all_branches = contents.get("all_branches", False)
+    source_module: Any = __import__(contents['source'], fromlist=['__trash'])
+    source_settings = source_module.Settings(**contents['source_settings'])
+    push_module: Any = __import__(contents['push'], fromlist=['__trash'])
+    push_settings = push_module.Settings(**contents['push_settings'])
+    include = re.compile(contents.get('include', ''))
+    exclude = re.compile(contents.get('exclude', '^$'))
+    all_branches = contents.get('all_branches', False)
     return Config(
         output_dir=output_dir,
         include=include,
